dfeaeef9f8c73df52d035ebecf511b624acdd7bc
Daniel Augustin <daniel.augustin1@gmx.de>
2020-07-10 20:18:55 -0700

    Improve naming in transform

--- PFERD/transform.py
+++ PFERD/transform.py
@@ -36,92 +36,131 @@ def apply_transform(
     """
 
     result: List[TF] = []
+
     for transformable in transformables:
         new_path = transform(transformable.path)
+
         if new_path:
             transformable.path = new_path
             result.append(transformable)
+
     return result
+
 
 # Transform combinators
 
 keep = lambda path: path
 
+
 def attempt(*args: Transform) -> Transform:
-    def inner(path: PurePath) -> Optional[PurePath]:
+    def apply_transforms_until_first_success(path: PurePath) -> Optional[PurePath]:
         for transform in args:
             result = transform(path)
+
             if result:
                 return result
+
         return None
-    return inner
+
+    return apply_transforms_until_first_success
+
 
 def optionally(transform: Transform) -> Transform:
     return attempt(transform, lambda path: path)
 
+
 def do(*args: Transform) -> Transform:
-    def inner(path: PurePath) -> Optional[PurePath]:
-        current = path
+    def apply_transforms_if_all_succeed(path: PurePath) -> Optional[PurePath]:
+        current_result = path
+
         for transform in args:
-            result = transform(current)
+            result = transform(current_result)
+
             if result:
-                current = result
+                current_result = result
             else:
                 return None
-        return current
-    return inner
+
+        return current_result
+
+    return apply_transforms_if_all_succeed
+
 
 def predicate(pred: Callable[[PurePath], bool]) -> Transform:
-    def inner(path: PurePath) -> Optional[PurePath]:
+    def apply_predicate(path: PurePath) -> Optional[PurePath]:
         if pred(path):
             return path
+
         return None
-    return inner
+
+    return apply_predicate
+
 
 def glob(pattern: str) -> Transform:
     return predicate(lambda path: path.match(pattern))
 
+
 def move_dir(source_dir: PathLike, target_dir: PathLike) -> Transform:
     source_path = to_path(source_dir)
     target_path = to_path(target_dir)
-    def inner(path: PurePath) -> Optional[PurePath]:
+
+    def make_child_of_target_dir(path: PurePath) -> Optional[PurePath]:
         if source_path in path.parents:
             return target_path / path.relative_to(source_path)
+
         return None
-    return inner
+
+    return make_child_of_target_dir
+
 
 def move(source: PathLike, target: PathLike) -> Transform:
     source_path = to_path(source)
     target_path = to_path(target)
-    def inner(path: PurePath) -> Optional[PurePath]:
+
+    def move_to_target(path: PurePath) -> Optional[PurePath]:
         if path == source_path:
             return target_path
+
         return None
-    return inner
+
+    return move_to_target
+
 
 def rename(source: str, target: str) -> Transform:
-    def inner(path: PurePath) -> Optional[PurePath]:
+    def rename_to_target_name(path: PurePath) -> Optional[PurePath]:
         if path.name == source:
             return path.with_name(target)
+
         return None
-    return inner
+
+    return rename_to_target_name
+
 
 def re_move(regex: Regex, target: str) -> Transform:
-    def inner(path: PurePath) -> Optional[PurePath]:
+    def move_if_match(path: PurePath) -> Optional[PurePath]:
         match = to_pattern(regex).fullmatch(str(path))
+
         if match:
             groups = [match.group(0)]
             groups.extend(match.groups())
+
             return PurePath(target.format(*groups))
+
         return None
-    return inner
+
+    return move_if_match
+
 
 def re_rename(regex: Regex, target: str) -> Transform:
-    def inner(path: PurePath) -> Optional[PurePath]:
+    def rename_if_match(path: PurePath) -> Optional[PurePath]:
         match = to_pattern(regex).fullmatch(path.name)
+
         if match:
             groups = [match.group(0)]
             groups.extend(match.groups())
+
             return path.with_name(target.format(*groups))
+
         return None
-    return inner
+
+    return rename_if_match
